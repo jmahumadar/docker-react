93a6fff712f1f39852b237106484eb3034da5b88
Jose Ahumada <JoseMiguel@ATCOMs-iMac.local>
2019-05-27 11:13:15 +1200

    initial commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi Jose, the app has changed again , check this out ASAP!!!
         </p>
         <a
           className="App-link"
